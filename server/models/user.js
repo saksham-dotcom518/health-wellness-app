b0eb123f838ee7c2ec84c83ab13454d86a03d089
Saksham <choudharysaksham255@gmail.com>
2026-05-03 00:46:32 +0530

    fix user schema

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,42 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    height: {
+      type: Number,
+      default: 170,
+    },
+
+    weight: {
+      type: Number,
+      default: 70,
+    },
+
+    age: {
+      type: Number,
+      default: 25,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
